0fdea6893599e1a01d5c293748cec3e9fb0248be
ss13412 <sjshao09@gmail.com>
2017-02-16 23:23:05 +0000

    enclose BP as a function and bugfix

--- src/TRPOCpuCode.c
+++ src/TRPOCpuCode.c
@@ -6,22 +6,49 @@
 #include "Maxfiles.h"
 #include "MaxSLiCInterface.h"
 
-int main()
-{
 
-        //////////////////// C Version of Back Propagation ////////////////////
-        
-        //////////////////// Memory Allocation ////////////////////
-        
-        // Number of Layers in the network
-        // e.g.: [Input] -----> [Hidden 1] -----> [Hidden 2] -----> [Output] is 4 layers.
-        const size_t NumLayers = 3;
+typedef struct {
+        // Input File Name
+        char * InFile;
+//        char * OutFile;
+
+
+        // Number of Layers in the network: [Input] --> [Hidden 1] --> [Hidden 2] --> [Output] is 4 layers.
+        size_t NumLayers;
         // Activation Function of each layer: t = tanh, l = linear (activate(x)=x), s = sigmoid
-        const char AcFunc [] = {'l', 's', 's'};
+        char * AcFunc;
         // Number of Nodes: from [Input] to [Output]
         // InvertedPendulum-v1: 4, 64, 64, 1
-        // Humanoid-v1: 367, 64, 64, 17
-        const size_t LayerSize [] = {2, 2, 2};
+        // Humanoid-v1: 367, 64, 64, 17        
+        size_t * LayerSize;
+        // Number of Samples
+        size_t NumSamples;
+        
+ 
+        
+} TRPOparam;
+
+
+int FVP (TRPOparam param, double * result, double * observ, double * mean, double * std) 
+{
+
+        //////////////////// Arguments ////////////////////
+        // param: TRPO parameters
+        // result: the Fisher-Vector Product
+        // observ: list of observations - corresponds to ob_no in modular_rl
+        // mean: list of mean values - corresponds to prob_np in modular_rl
+        // std: list of std values - in TRPO paper std is fixed for all samples in an iteration
+        // W: Weights in the neural network         double * W [NumLayers-1];
+
+        // Assign Parameters
+        const size_t NumLayers = param.NumLayers;
+        char * AcFunc = param.AcFunc;
+        size_t * LayerSize = param.LayerSize;
+        const size_t NumSamples = param.NumSamples;
+
+
+        
+        //////////////////// Memory Allocation ////////////////////
 
         // Allocate Memory for each layer and the Gradient w.r.t. the pre-activation values in each layer
         // Remarks: This gradient storage is optional, as we can probably overwrite the values in Layer[i]
@@ -39,6 +66,7 @@ int main()
         // W[i] and B[i] is the Weight Matrix and Bias Vector from Layer[i] to Layer[i+1]
         // Item (j,k) in W[i] refers to the weight from Neuron #j in Layer[i] to Neuron #k in Layer[i+1]
         // The Gradient for W[i] and B[i] are dW[i] and dB[i]
+        // Remarks: B[i]
         double * W [NumLayers-1];
         double * dW [NumLayers-1];
         double * B [NumLayers-1];
@@ -50,18 +78,11 @@ int main()
                 dB[i] = (double *) calloc(LayerSize[i+1], sizeof(double));
         }
         
+
         
         //////////////////// Initialisation ////////////////////
         
-        // Assign Input Values to Layer[0]
-//        for (size_t i=0; i<LayerSize[0]; ++i) Layer[0][i] = i;
-
-        // ------------- Simple Test Case ------------- //
-        // Simple Test Case: 2-2-2 Network with sigmoid
-        
-        // Assign Input Values
-        Layer[0][0] = 0.05;
-        Layer[0][1] = 0.10;
+        // TODO Init Weights and Bias from File
         
         // Assign Weights
         W[0][0*2+0] = 0.15;
@@ -73,113 +94,120 @@ int main()
         W[1][0*2+1] = 0.50;
         W[1][1*2+0] = 0.45;
         W[1][1*2+1] = 0.55;
-        
+
         // Assign Bias
         B[0][0] = 0.35;
         B[0][1] = 0.35;
         B[1][0] = 0.60;
-        B[1][1] = 0.60;
+        B[1][1] = 0.60;        
         
-        // ------------- Simple Test Case ------------- //
-
-
-        //////////////////// Forward Propagation ////////////////////
         
-        for (size_t i=0; i<NumLayers-1; ++i) {
-                // Propagate from Layer[i] to Layer[i+1]
-                for (size_t j=0; j<LayerSize[i+1]; ++j) {
-                        // Calculating item[j] in next layer
-                        Layer[i+1][j] = B[i][j];
-//                        printf("Net[%zu][%zu] init to bias %f \n", i+1, j, Layer[i+1][j]);
-                        for (size_t k=0; k<LayerSize[i]; ++k) {
-                                // From Neuron #k in Layer[i] to Neuron #j in Layer[i+1]
-                                Layer[i+1][j] += Layer[i][k] * W[i][k*LayerSize[i+1]+j];
+        //////////////////// Main Loop Over All Samples ////////////////////        
+        
+        for (size_t iter=0; iter<NumSamples; iter++) {
+        
+                //////////////////// Forward Propagation ////////////////////
+        
+                // Assign Input Values
+                for (size_t i=0; i<LayerSize[0]; ++i) Layer[0][i] = observ[iter*LayerSize[0]+i];       
+        
+                // Forward Propagation
+                for (size_t i=0; i<NumLayers-1; ++i) {
+                        
+                        // Propagate from Layer[i] to Layer[i+1]
+                        for (size_t j=0; j<LayerSize[i+1]; ++j) {
+                                
+                                // Calculating item[j] in next layer
+                                Layer[i+1][j] = B[i][j];
+//                              printf("Net[%zu][%zu] init to bias %f \n", i+1, j, Layer[i+1][j]);
+                                for (size_t k=0; k<LayerSize[i]; ++k) {
+                                        // From Neuron #k in Layer[i] to Neuron #j in Layer[i+1]
+                                        Layer[i+1][j] += Layer[i][k] * W[i][k*LayerSize[i+1]+j];
 //                              printf("Net[%zu][%zu] += Input %f * Weight %f => %f \n", i+1, j, Layer[i][k], W[i][k*LayerSize[i+1]+j], Layer[i+1][j]);
+                                }
+//                              printf("Net[%zu][%zu] = %f => ", i+1, j, Layer[i+1][j]);
+                        
+                                 // Apply Activation Function
+                                switch (AcFunc[i+1]) {
+                                        // Linear Activation Function: Ac(x) = (x)
+                                        case 'l': {break;}
+                                        // tanh() Activation Function
+                                        case 't': {Layer[i+1][j] = tanh(Layer[i+1][j]); break;}
+                                        // sigmoid Activation Function
+                                        case 's': {Layer[i+1][j] = 1.0/(1+exp(-Layer[i+1][j])); break;}
+                                        // Default: Activation Function not supported
+                                        default: {
+                                                printf("[ERROR] Activation Function for Layer [%zu] is %c. Unsupported.\n", i+1, AcFunc[i+1]);
+                                                return -1;
+                                        }
+                                }
+//                              printf("Out[%zu][%zu] = %f \n", i+1, j, Layer[i+1][j]);
                         }
-//                        printf("Net[%zu][%zu] = %f => ", i+1, j, Layer[i+1][j]);
-                        // Apply Activation Function
-                        switch (AcFunc[i+1]) {
-                                // Linear Activation Function: Ac(x) = (x)
-                                case 'l': {break;}
-                                // tanh() Activation Function
-                                case 't': {Layer[i+1][j] = tanh(Layer[i+1][j]); break;}
-                                // sigmoid Activation Function
-                                case 's': {Layer[i+1][j] = 1.0/(1+exp(-Layer[i+1][j])); break;}
-                                // Default: Activation Function not supported
-                                default: {
-                                        printf("[ERROR] Activation Function for Layer [%zu] is %c. Unsupported.\n", i+1, AcFunc[i+1]);
-                                        exit(1);
+                }
+                
+                // Final Output
+                for (size_t i=0; i<LayerSize[NumLayers-1]; ++i) {
+                        printf("output[%zu] = %f \n", i, Layer[NumLayers-1][i]);
+                }
+
+                // TODO: *0.1 in the final layer for TRPO
+
+                
+                //////////////////// Backward Propagation ////////////////////                 
+
+        
+                // Assign the derivative of Loss Func w.r.t. the output values from the final layer
+                // TODO Assign according to KL Divergence
+                GLayer[NumLayers-1][0] = Layer[NumLayers-1][0] - 0.01;
+                GLayer[NumLayers-1][1] = Layer[NumLayers-1][1] - 0.99;                
+
+                
+                // Backward Propagation
+                for (size_t i=NumLayers-1; i>0; --i) {
+       
+                        // Propagate from Layer[i] to Layer[i-1]
+                        for (size_t j=0; j<LayerSize[i]; ++j) {
+
+                                // Calculate derivative of the activation function
+                                switch (AcFunc[i]) {
+                                        // Linear Activation Function: Ac(x) = (x)
+                                        case 'l': {break;}
+                                        // tanh() Activation Function: tanh' = 1 - tanh^2
+                                        case 't': {GLayer[i][j] = GLayer[i][j] * (1- Layer[i][j] * Layer[i][j]); break;}
+                                        // sigmoid Activation Function: sigmoid' = sigmoid * (1 - sigmoid)
+                                        case 's': {GLayer[i][j] = GLayer[i][j] * Layer[i][j] * (1- Layer[i][j]); break;}
+                                        // Default: Activation Function not supported
+                                        default: {
+                                                fprintf(stderr, "[ERROR] Activation Function for Layer [%zu] is %c. Unsupported.\n", i+1, AcFunc[i]);
+                                                return -1;
+                                        }
+                                }
+                                
+                                // The derivative w.r.t to Bias is the same as that w.r.t. the pre-activated value
+                                dB[i-1][j] = GLayer[i][j];
+                        }
+                
+                        // Calculate the derivative w.r.t. to Weight
+                        for (size_t j=0; j<LayerSize[i-1]; ++j) {
+                                for (size_t k=0; k<LayerSize[i]; ++k) {
+                                        // The Derivative w.r.t. to the weight from Neuron #j in Layer[i-1] to Neuron #k in Layer[i]
+                                        dW[i-1][j*LayerSize[i]+k] = GLayer[i][k] * Layer[i-1][j];
+                                        printf("Gradient w.r.t. Weight[%zu][%zu][%zu] = %f \n", i-1, j, k, dW[i-1][j*LayerSize[i]+k]);
                                 }
                         }
-//                        printf("Out[%zu][%zu] = %f \n", i+1, j, Layer[i+1][j]);
-                }
-        }
-        
-        // Output
-        for (size_t i=0; i<LayerSize[NumLayers-1]; ++i) {
-                printf("output[%zu] = %f \n", i, Layer[NumLayers-1][i]);
-        }
-
-        // TODO: *0.1 in the final layer for TRPO
-        
-        
-        //////////////////// Backward Propagation ////////////////////        
-        
-        // TODO: Loss Function
-        
-        // Simple Test Case: 2-2-2 Network with sigmoid
-        // Assign the derivative of Loss Func w.r.t. output layer
-        //        for (size_t i=0; i<LayerSize[NumLayers-1]; ++i) GLayer[NumLayers-1][i] = i;
-        
-        // ------------- Simple Test Case ------------- //
-        
-        // Assign the derivative of Loss Func w.r.t. the output values from the final layer   
-        GLayer[NumLayers-1][0] = Layer[NumLayers-1][0] - 0.01;
-        GLayer[NumLayers-1][1] = Layer[NumLayers-1][1] - 0.99;
-        
-        for (size_t i=NumLayers-1; i>0; --i) {
-                // Propagate from Layer[i] to Layer[i-1]
                 
-                // Calculate derivative of the activation function
-                for (size_t j=0; j<LayerSize[i]; ++j) {
-                        switch (AcFunc[i]) {
-                                // Linear Activation Function: Ac(x) = (x)
-                                case 'l': {break;}
-                                // tanh() Activation Function: tanh' = 1 - tanh^2
-                                case 't': {GLayer[i][j] = GLayer[i][j] * (1- Layer[i][j] * Layer[i][j]); break;}
-                                // sigmoid Activation Function: sigmoid' = sigmoid * (1 - sigmoid)
-                                case 's': {GLayer[i][j] = GLayer[i][j] * Layer[i][j] * (1- Layer[i][j]); break;}
-                                // Default: Activation Function not supported
-                                default: {
-                                        printf("[ERROR] Activation Function for Layer [%zu] is %c. Unsupported.\n", i+1, AcFunc[i]);
-                                        exit(1);
+                        // Calculate the derivative w.r.t. the output values from Layer[i]
+                        for (size_t j=0; j<LayerSize[i-1]; ++j) {
+                                GLayer[i-1][j] = 0;
+                                for (size_t k=0; k<LayerSize[i]; ++k) {
+                                        // Accumulate the Gradient from Neuron #k in Layer[i] to Neuron #j in Layer[i-1]
+                                        GLayer[i-1][j] += GLayer[i][k] * W[i-1][j*LayerSize[i]+k];
                                 }
                         }
-                        // The derivative w.r.t to Bias is the same as that w.r.t. the pre-activated value
-                        dB[i][j] = GLayer[i][j];
-                }
                 
-                // Calculate the derivative w.r.t. to Weight
-                for (size_t j=0; j<LayerSize[i-1]; ++j) {
-                        for (size_t k=0; k<LayerSize[i]; ++k) {
-                                // The Derivative w.r.t. to the weight from Neuron #j in Layer[i-1] to Neuron #k in Layer[i]
-                                dW[i-1][j*LayerSize[i]+k] = GLayer[i][k] * Layer[i-1][j];
-                                printf("Gradient w.r.t. Weight[%zu][%zu][%zu] = %f \n", i-1, j, k, dW[i-1][j*LayerSize[i]+k]);
-                        }
-                }
-                
-                // Calculate the derivative w.r.t. the output values from Layer[i]
-                for (size_t j=0; j<LayerSize[i-1]; ++j) {
-                        GLayer[i-1][j] = 0;
-                        for (size_t k=0; k<LayerSize[i]; ++k) {
-                                // Accumulate the Gradient from Neuron #k in Layer[i] to Neuron #j in Layer[i-1]
-                                GLayer[i-1][j] += GLayer[i][k] * W[i-1][j*LayerSize[i]+k];
-                        }
-                }
-                
-        }
-
-
+                } // End of Back Propagation
+        
+        } // End of iteration over current sample
 
 
         //////////////////// Clean Up ////////////////////  
@@ -189,8 +217,39 @@ int main()
         for (size_t i=0; i<NumLayers-1; ++i) {free(W[i]); free(dW[i]); free(B[i]); free(dB[i]);}
 
 
+        return 0;
+}
 
-        // FPGA Stuff below
+
+
+
+int main()
+{
+
+        //////////////////// C Version of Back Propagation ////////////////////
+        
+        // Simple Data 2-2-2
+        char AcFunc [] = {'l', 's', 's'};
+        size_t LayerSize [] = {2, 2, 2};
+
+        TRPOparam SimpleDataParam;
+        SimpleDataParam.InFile = "SimpleData.txt";
+        SimpleDataParam.NumLayers = 3;
+        SimpleDataParam.AcFunc = AcFunc;
+        SimpleDataParam.LayerSize = LayerSize;
+        SimpleDataParam.NumSamples = 1;
+
+        double dummy1=0;
+        double dummy2=1;
+        double dummy3=2;
+        double observ [] = {0.05, 0.10};
+
+        int FVPStatus = FVP (SimpleDataParam, &dummy1, observ, &dummy2, &dummy3);
+
+
+
+        //////////////////// FPGA ////////////////////
+
 
         const int inSize = 384;
 
@@ -206,7 +265,7 @@ int main()
         }
 
         printf("Running on DFE.\n");
-        Demo(inSize, a, b, out);
+        TRPO(inSize, a, b, out);
 
 
   /***
